6d6266413387e9357436d6c529d335aa6b8bb18b
Claudio Beatrice <claudi0.beatric3@gmail.com>
2024-08-01 00:04:33 +0200

    chore: remove kubernetes scripts and targets

--- kubernetes.mk
+++
@@ -1,5 +0,0 @@
-KUBERNETES_SHELLBITS_DIR=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-
-.PHONY: generate-ts-client
-generate-ts-client:
-	@${KUBERNETES_SHELLBITS_DIR}/kubernetes/generate-ts-client.sh
