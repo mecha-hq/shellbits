2de6e397371a1cfe3a9ce0f4a79d548280dd23fe
Claudio Beatrice <claudi0.beatric3@gmail.com>
2024-11-08 16:47:16 +0100

    fix: add tools install to script

--- mise/update-tools.sh
+++ mise/update-tools.sh
@@ -5,6 +5,7 @@
 set -e
 set -o errexit -o nounset
 
+mise install && \
 yq -M '.tools | keys | .[]' .mise.toml -oy | \
 xargs -I {} -n 1 sh -c 'echo {} = \"$(mise latest {} 2>/dev/null)\"' > .mise.tools.toml && \
 sed -i.bak '/\[tools\]/,/^$/{
