05eb6adc74ba2bdc0fd2185c800abd5b400e10d8
omissis <claudi0.beatric3@gmail.com>
2026-02-21 17:23:50 +0100

    Refactor json scripts

---
+++ core/json/env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+set -u
+
+# Common public environment variables for json scripts
+
+# Export environment variables
+# No common environment variables needed for json scripts
---
+++ core/json/lint/main.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+set -o errexit -o nounset
+
+# Variables
+_script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+# Source environment variables
+. "${_script_dir}/env.sh"
+
+# Exec
+find . \
+  -type f \
+  -not -path "*/.git" \
+  -not -path "*/.github" \
+  -not -path "*/.go/*" \
+  -not -path "*/.vscode" \
+  -not -path "*/.idea" \
+  -name "*.json" \
+  -exec jsonlint -c -q -t '  ' {} \;
