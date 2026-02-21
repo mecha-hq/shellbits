fa116460c2128a81a8b9c589dc36cc6efb77c44f
omissis <claudi0.beatric3@gmail.com>
2026-02-21 15:23:54 +0100

    refactor: update Makefile to reflect new folder structure and ensure POSIX compliance

--- core/tilt.mk
+++ core/tilt.mk
@@ -2,18 +2,18 @@ TILT_SHELLBITS_DIR=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 .PHONY: tilt-down
 tilt-down:
-	@${TILT_SHELLBITS_DIR}/tilt/down.sh \
-	   $$([[ "${TILT_DOWN_FORCE:-${TILT_FORCE:-$${FORCE:-0}}}" -eq 1 ]] && echo --force)
+	@${TILT_SHELLBITS_DIR}/tilt/down/main.sh \
+	   $$([ "${TILT_DOWN_FORCE:-${TILT_FORCE:-${FORCE:-0}}}" -eq 1 ] && echo --force)
 
 .PHONY: tilt-start
 tilt-start:
-	@${TILT_SHELLBITS_DIR}/tilt/start.sh
+	@${TILT_SHELLBITS_DIR}/tilt/start/main.sh
 
 .PHONY: tilt-stop
 tilt-stop:
-	@${TILT_SHELLBITS_DIR}/tilt/stop.sh
+	@${TILT_SHELLBITS_DIR}/tilt/stop/main.sh
 
 .PHONY: tilt-up
 tilt-up:
-	@${TILT_SHELLBITS_DIR}/tilt/up.sh \
-	   $$([[ "${TILT_UP_FORCE:-${TILT_FORCE:-$${FORCE:-0}}}" -eq 1 ]] && echo --force)
+	@${TILT_SHELLBITS_DIR}/tilt/up/main.sh \
+	   $$([ "${TILT_UP_FORCE:-${TILT_FORCE:-${FORCE:-0}}}" -eq 1 ] && echo --force)
