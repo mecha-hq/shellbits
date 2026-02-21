b7f774df8ec49122edf5eadf234258174c33989c
omissis <claudi0.beatric3@gmail.com>
2026-02-21 19:36:03 +0100

    fix(goreleaser): cleanup commands from unused code.

--- core/goreleaser/release/main.sh
+++ core/goreleaser/release/main.sh
@@ -2,18 +2,10 @@
 
 set -eu
 
-# Variables
-_script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-
-# Source environment variables
-. "${_script_dir}/env.sh"
-
-# Get Go version
-_go_version=$(go version | cut -d ' ' -f 3)
-
-export GO_VERSION="${_go_version}"
+export GO_VERSION="$(go version | cut -d ' ' -f 3)"
 
 # Exec
+
 goreleaser check
 goreleaser release --debug --clean
 
