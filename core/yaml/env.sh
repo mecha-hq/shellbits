1aade8ec9c9c71b9a524bcd3ccdebb923728d2be
omissis <claudi0.beatric3@gmail.com>
2026-02-21 18:31:41 +0100

    chore(yaml): simplify code

--- core/yaml/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-set -u
-
-# Common public environment variables for yaml scripts
-
-# Export environment variables
-# No common environment variables needed for yaml scripts
--- core/yaml/format/main.sh
+++ core/yaml/format/main.sh
@@ -3,11 +3,6 @@
 set -e
 set -o errexit -o nounset
 
-# Variables
-_script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-
-# Source environment variables
-. "${_script_dir}/env.sh"
-
 # Exec
+
 find . \( -name '*.yaml' -o -name '*.yml' \) -type f -exec yq eval -P -I 2 -M -i {} \;
