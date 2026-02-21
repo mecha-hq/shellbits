5de7d0233642e7aef4e9277f9eef09e58c8f3351
omissis <claudi0.beatric3@gmail.com>
2026-02-21 17:24:53 +0100

    Refactor shell scripts

---
+++ core/shell/env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+set -u
+
+# Common public environment variables for shell scripts
+
+# Export environment variables
+# No common environment variables needed for shell scripts
---
+++ core/shell/lint/main.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+set -o errexit -o nounset
+
+# Variables
+_script_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+# Source environment variables
+. "${_script_dir}/env.sh"
+
+# Exec
+shellcheck ${SHELL_LINT_ARGS:- -a -o all -s bash} -- **/*.sh
