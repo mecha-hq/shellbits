df148d990e9e46139ebac10631d9749f0ede5b4a
omissis <claudi0.beatric3@gmail.com>
2026-02-21 18:27:30 +0100

    chore: remove asdf and brew support

--- core/asdf.mk
+++
@@ -1,9 +0,0 @@
-ASDF_SHELLBITS_DIR=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-
-.PHONY: asdf-add-plugins
-asdf-add-plugins:
-	@${ASDF_SHELLBITS_DIR}/asdf/add-plugins.sh
-
-.PHONY: asdf-update-tools
-asdf-update-tools:
-	@${ASDF_SHELLBITS_DIR}/asdf/update-tools.sh
--- core/brew.mk
+++
@@ -1,5 +0,0 @@
-BREW_SHELLBITS_DIR=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-
-.PHONY: brew-install-tools
-brew-install-tools:
-	@${BREW_SHELLBITS_DIR}/brew/install-tools.sh
